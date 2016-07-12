23f12ec61f89c6fbd2f83ac6e56153cf77d55dc0
sstone <sstone@users.noreply.github.com>
2016-07-12 11:02:47 +0200

    add test vector for deterministic signatures

--- src/test/scala/fr/acinq/bitcoin/CryptoSpec.scala
+++ src/test/scala/fr/acinq/bitcoin/CryptoSpec.scala
@@ -49,4 +49,39 @@ class CryptoSpec extends FlatSpec {
     val encoded = Crypto.encodeSignature(r, s)
     assert(Crypto.verifySignature(data, encoded, publicKey))
   }
+
+  it should "generate deterministic signatures" in {
+    // dataset from https://bitcointalk.org/index.php?topic=285142.msg3299061#msg3299061
+    val dataset = Seq(
+      (
+        "0000000000000000000000000000000000000000000000000000000000000001",
+        "Satoshi Nakamoto",
+        "3045022100934b1ea10a4b3c1757e2b0c017d0b6143ce3c9a7e6a4a49860d7a6ab210ee3d802202442ce9d2b916064108014783e923ec36b49743e2ffa1c4496f01a512aafd9e5"
+        ),
+      ("0000000000000000000000000000000000000000000000000000000000000001",
+        "Everything should be made as simple as possible, but not simpler.",
+        "3044022033a69cd2065432a30f3d1ce4eb0d59b8ab58c74f27c41a7fdb5696ad4e6108c902206f807982866f785d3f6418d24163ddae117b7db4d5fdf0071de069fa54342262"
+        ),
+      ("0000000000000000000000000000000000000000000000000000000000000001",
+        "All those moments will be lost in time, like tears in rain. Time to die...",
+        "30450221008600dbd41e348fe5c9465ab92d23e3db8b98b873beecd930736488696438cb6b0220547fe64427496db33bf66019dacbf0039c04199abb0122918601db38a72cfc21"
+        ),
+      ("FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEBAAEDCE6AF48A03BBFD25E8CD0364140",
+        "Satoshi Nakamoto",
+        "3045022100fd567d121db66e382991534ada77a6bd3106f0a1098c231e47993447cd6af2d002206b39cd0eb1bc8603e159ef5c20a5c8ad685a45b06ce9bebed3f153d10d93bed5"
+        ),
+      ("f8b8af8ce3c7cca5e300d33939540c10d45ce001b8f252bfbc57ba0342904181",
+        "Alan Turing",
+        "304402207063ae83e7f62bbb171798131b4a0564b956930092b33b07b395615d9ec7e15c022058dfcc1e00a35e1572f366ffe34ba0fc47db1e7189759b9fb233c5b05ab388ea"),
+      ("e91671c46231f833a6406ccbea0e3e392c76c167bac1cb013f6f1013980455c2",
+        "There is a computer disease that anybody who works with computers knows about. It's a very serious disease and it interferes completely with the work. The trouble with computers is that you 'play' with them!",
+        "3045022100b552edd27580141f3b2a5463048cb7cd3e047b97c9f98076c32dbdf85a68718b0220279fa72dd19bfae05577e06c7c0c1900c371fcd5893f7e1d56a37d30174671f6")
+    )
+
+    dataset.map {
+      case (k, m, s) =>
+        val sig: BinaryData = Crypto.encodeSignature(Crypto.sign(Crypto.sha256(BinaryData(m.getBytes("UTF-8"))), BinaryData(k)))
+        assert(sig == BinaryData(s))
+    }
+  }
 }
