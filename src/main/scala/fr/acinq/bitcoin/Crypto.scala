2826db7ec4db07e2d6d27f26f2610755ac54617b
sstone <sstone@users.noreply.github.com>
2017-02-08 14:22:42 +0100

    minor cleanup of isDERSignature

--- src/main/scala/fr/acinq/bitcoin/Crypto.scala
+++ src/main/scala/fr/acinq/bitcoin/Crypto.scala
@@ -12,7 +12,6 @@ import org.bouncycastle.crypto.params.{ECDomainParameters, ECPrivateKeyParameter
 import org.bouncycastle.crypto.signers.{ECDSASigner, HMacDSAKCalculator}
 import org.bouncycastle.math.ec.ECPoint
 
-import scala.annotation.tailrec
 
 object Crypto {
   val params = SECNamedCurves.getByName("secp256k1")
@@ -126,6 +125,7 @@ object Crypto {
     override def toString = toBin(true).toString
   }
 
+
   object Point {
     def apply(data: BinaryData): Point = Point(curve.getCurve.decodePoint(data))
   }
@@ -236,24 +236,68 @@ object Crypto {
   def encodeSignature(t: (BigInteger, BigInteger)): BinaryData = encodeSignature(t._1, t._2)
 
   def isDERSignature(sig: Seq[Byte]): Boolean = {
-    require(sig.length >= 9 && sig.length <= 73)
-    require(sig(0) == 0x30.toByte)
-    require(sig(1) == sig.length - 3)
-    require(sig(2) == 0x02.toByte)
+    // Format: 0x30 [total-length] 0x02 [R-length] [R] 0x02 [S-length] [S] [sighash]
+    // * total-length: 1-byte length descriptor of everything that follows,
+    //   excluding the sighash byte.
+    // * R-length: 1-byte length descriptor of the R value that follows.
+    // * R: arbitrary-length big-endian encoded R value. It must use the shortest
+    //   possible encoding for a positive integers (which means no null bytes at
+    //   the start, except a single one when the next byte has its highest bit set).
+    // * S-length: 1-byte length descriptor of the S value that follows.
+    // * S: arbitrary-length big-endian encoded S value. The same rules apply.
+    // * sighash: 1-byte value indicating what data is hashed (not part of the DER
+    //   signature)
 
+    // Minimum and maximum size constraints.
+    if (sig.size < 9) return false
+    if (sig.size > 73) return false
+
+    // A signature is of type 0x30 (compound).
+    if (sig(0) != 0x30.toByte) return false
+
+    // Make sure the length covers the entire signature.
+    if (sig(1) != sig.size - 3) return false
+
+    // Extract the length of the R element.
     val lenR = sig(3)
-    require(lenR > 0 && lenR + 5 < sig.length)
-    require((sig(4) & 0x80) == 0)
-    if (lenR > 1 && sig(4) == 0) require((sig(5) & 0x80) != 0)
 
-    require(sig(lenR + 4) == 0x02.toByte)
-    val lenS = sig(lenR + 5)
-    require(lenS > 0)
-    require(lenR + lenS + 7 == sig.length)
-    require((sig(lenR + 6) & 0x80) == 0)
-    if (lenS > 1 && sig(lenR + 6) == 0) require((sig(lenR + 7) & 0x80) != 0)
+    // Make sure the length of the S element is still inside the signature.
+    if (5 + lenR >= sig.size) return false
 
-    true
+    // Extract the length of the S element.
+    val lenS = sig(5 + lenR)
+
+    // Verify that the length of the signature matches the sum of the length
+    // of the elements.
+    if (lenR + lenS + 7 != sig.size) return false
+
+    // Check whether the R element is an integer.
+    if (sig(2) != 0x02) return false
+
+    // Zero-length integers are not allowed for R.
+    if (lenR == 0) return false
+
+    // Negative numbers are not allowed for R.
+    if ((sig(4) & 0x80.toByte) != 0) return false
+
+    // Null bytes at the start of R are not allowed, unless R would
+    // otherwise be interpreted as a negative number.
+    if (lenR > 1 && (sig(4) == 0x00) && (sig(5) & 0x80) == 0) return false
+
+    // Check whether the S element is an integer.
+    if (sig(lenR + 4) != 0x02.toByte) return false
+
+    // Zero-length integers are not allowed for S.
+    if (lenS == 0) return false
+
+    // Negative numbers are not allowed for S.
+    if ((sig(lenR + 6) & 0x80) != 0) return false
+
+    // Null bytes at the start of S are not allowed, unless S would otherwise be
+    // interpreted as a negative number.
+    if (lenS > 1 && (sig(lenR + 6) == 0x00) && (sig(lenR + 7) & 0x80) == 0) return false
+
+    return true
   }
 
   def isLowDERSignature(sig: Seq[Byte]): Boolean = isDERSignature(sig) && {
