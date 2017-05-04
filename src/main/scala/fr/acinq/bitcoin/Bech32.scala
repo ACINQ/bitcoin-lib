c2edd2b73e4e5881105e143f26bfa575547c81a2
sstone <sstone@users.noreply.github.com>
2017-05-04 11:20:15 +0200

    Bech32: minor improvements (doc, explicit witness version parameter...)

--- src/main/scala/fr/acinq/bitcoin/Bech32.scala
+++ src/main/scala/fr/acinq/bitcoin/Bech32.scala
@@ -1,16 +1,24 @@
 package fr.acinq.bitcoin
 
 /**
-  * Created by fabrice on 19/04/17.
+  * See https://github.com/sipa/bech32/blob/master/bip-witaddr.mediawiki
   */
 object Bech32 {
   val alphabet = "qpzry9x8gf2tvdw0s3jn54khce6mua7l"
-  val map = alphabet.zipWithIndex.toMap
-  val pam = map.map(_.swap)
 
-  def expand(hrp: String): Seq[Byte] = hrp.map(c => (c.toInt >>> 5).toByte) ++ (0.toByte +: hrp.map(c => (c.toInt & 31).toByte))
+  // 5 bits integer
+  // Bech32 works with 5bits values, we use this type to make it explicit: whenever you see Int5 it means 5bits values, and
+  // whenever you see Byte it means 8bits values
+  type Int5 = Byte
 
-  def polymod(values: Seq[Byte]): Int = {
+  // char -> 5 bits value
+  val map: Map[Char, Int5] = alphabet.zipWithIndex.toMap.mapValues(_.toByte)
+  // 5 bits value -> char
+  val pam: Map[Int5, Char] = map.map(_.swap)
+
+  def expand(hrp: String): Seq[Int5] = hrp.map(c => (c.toInt >>> 5).toByte) ++ (0.toByte +: hrp.map(c => (c.toInt & 31).toByte))
+
+  def polymod(values: Seq[Int5]): Int = {
     val GEN = Seq(0x3b6a57b2, 0x26508e6d, 0x1ea119fa, 0x3d4233dd, 0x2a1462b3)
     var chk = 1
     values.map(v => {
@@ -23,23 +31,39 @@ object Bech32 {
     chk
   }
 
-  def decode(input1: String): (String, Seq[Byte]) = {
-    val input = input1.toLowerCase()
+  /**
+    * decodes a bech32 string
+    * @param bech32 bech32 string
+    * @return a (hrp, data) tuple
+    */
+  def decode(bech32: String): (String, Seq[Int5]) = {
+    val input = bech32.toLowerCase()
     val pos = input.lastIndexOf('1')
     val hrp = input.take(pos)
-    val data = input.drop(pos + 1).map(c => map(c).toByte)
+    val data = input.drop(pos + 1).map(c => map(c))
     val checksum = polymod(expand(hrp) ++ data)
-    require(checksum == 1, s"invalid checksum for $input1")
+    require(checksum == 1, s"invalid checksum for $bech32")
     (hrp, data.dropRight(6))
   }
 
-  def checksum(hrp: String, data : Seq[Byte]): Seq[Byte] = {
+  /**
+    *
+    * @param hrp Human Readable Part
+    * @param data data (a sequence of 5 bits integers)
+    * @return a checksum computed over hrp and data
+    */
+  def checksum(hrp: String, data : Seq[Int5]): Seq[Int5] = {
     val values = expand(hrp) ++ data
     val poly = polymod(values ++ Seq(0.toByte,0.toByte,0.toByte,0.toByte,0.toByte,0.toByte)) ^ 1.toByte
     for(i <- 0 to 5) yield ((poly >>> 5 * (5 - i)) & 31).toByte
   }
 
-  def eight2five(input: Seq[Byte]): Seq[Byte] = {
+  /**
+    *
+    * @param input a sequence of 8 bits integers
+    * @return a sequence of 5 bits integers
+    */
+  def eight2five(input: Seq[Byte]): Seq[Int5] = {
     var buffer = 0L
     val output = collection.mutable.ArrayBuffer.empty[Byte]
     var count = 0
@@ -55,7 +79,12 @@ object Bech32 {
     output
   }
 
-  def five2eight(input: Seq[Byte]): Seq[Byte] = {
+  /**
+    *
+    * @param input a sequence of 5 bits integers
+    * @return a sequence of 8 bits integers
+    */
+  def five2eight(input: Seq[Int5]): Seq[Byte] = {
     var buffer = 0L
     val output = collection.mutable.ArrayBuffer.empty[Byte]
     var count = 0
@@ -72,22 +101,36 @@ object Bech32 {
     output
   }
 
-  def encodeAddress(hrp: String, data: BinaryData) : String = {
+  /**
+    * encode a bitcoin witness address
+    * @param hrp should be "bc" or "tb"
+    * @param witnessVersion witness version (0 to 16, only 0 is currently defined)
+    * @param data witness program: if version is 0, either 20 bytes (P2WPKH) or 32 bytes (P2WSH)
+    * @return a bech32 encoded witness address
+    */
+  def encodeWitnessAddress(hrp: String, witnessVersion: Byte, data: BinaryData) : String = {
     // prepend witness version: 0
-    val data1 = 0.toByte +: Bech32.eight2five(data)
+    val data1 = witnessVersion +: Bech32.eight2five(data)
     val checksum = Bech32.checksum(hrp, data1)
     hrp + "1" + new String((data1 ++ checksum).map(i => Bech32.pam(i)).toArray)
   }
 
-  def decodeAddress(address: String): BinaryData = {
+  /**
+    * decode a bitcoin witness address
+    * @param address witness address
+    * @return a (version, program) tuple where version is the witness version and program the decoded witness program.
+    *         If version is 0, it will be either 20 bytes (P2WPKH) or 32 bytes (P2WSH)
+    */
+  def decodeWitnessAddress(address: String): (Byte, BinaryData) = {
     if (address.indexWhere(_.isLower) != -1 && address.indexWhere(_.isUpper) != -1) throw new IllegalArgumentException("input mixes lowercase and uppercase characters")
     val (hrp, data) = decode(address)
     require(hrp == "bc" || hrp == "tb", s"invalid HRP $hrp")
-    require(data(0) >= 0 && data(0) <= 16, "invalid segwit version")
+    val version = data(0)
+    require(version >= 0 && version <= 16, "invalid segwit version")
     val bin = five2eight(data.drop(1))
     require(bin.length >= 2 && bin.length <= 40, s"invalid witness program length ${bin.length}")
-    if (data(0) == 0) require(bin.length == 20 || bin.length == 32, s"invalid witness program length ${bin.length}")
-    bin
+    if (version == 0) require(bin.length == 20 || bin.length == 32, s"invalid witness program length ${bin.length}")
+    (version, bin)
   }
 
 }
