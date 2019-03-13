28707d15ad573c70160efd1b7f6661d9d09ca7de
pm47 <pm.padiou@gmail.com>
2019-03-13 17:04:18 +0100

    define ByteVector32.toString == toHex

--- src/main/scala/fr/acinq/bitcoin/Protocol.scala
+++ src/main/scala/fr/acinq/bitcoin/Protocol.scala
@@ -16,6 +16,8 @@ case class ByteVector32(bytes: ByteVector) {
   require(bytes.size == 32, s"size must be 32 bytes, is ${bytes.size} bytes")
 
   def reverse: ByteVector32 = ByteVector32(bytes.reverse)
+
+  override def toString: String = bytes.toHex
 }
 
 object ByteVector32 {
