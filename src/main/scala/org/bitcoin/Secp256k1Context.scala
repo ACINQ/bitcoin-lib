a39d923f358e1e62683db812d3397bad9b5ebc42
sstone <sstone@users.noreply.github.com>
2019-03-13 10:26:28 +0100

    Don't use our native library extractor on Android
    Embedding native libraries and extracting them will not work on Android. Instead, developpers need to compile
    secp256k1 in their project, and load it with System.loadLibrary("secp256k1").
    
    This too is copied from xerial-jdbc.

--- src/main/scala/org/bitcoin/Secp256k1Context.scala
+++ src/main/scala/org/bitcoin/Secp256k1Context.scala
@@ -1,16 +1,20 @@
 package org.bitcoin
 
 abstract class Secp256k1Context {
-  @native def secp256k1_init_context() : Long
+  @native def secp256k1_init_context(): Long
 }
 
-object Secp256k1Context extends Secp256k1Context  {
+object Secp256k1Context extends Secp256k1Context {
   val (isEnabled, context): (Boolean, Long) = try {
-    fr.acinq.Secp256k1Loader.initialize()
+    if ("The Android Project".equals(System.getProperty("java.vm.vendor"))) {
+      System.loadLibrary("secp256k1")
+    } else {
+      fr.acinq.Secp256k1Loader.initialize()
+    }
     (true, secp256k1_init_context())
   } catch {
     case t: Throwable => (false, -1L)
   }
 
-  def getContext() : Long = context
+  def getContext(): Long = context
 }
