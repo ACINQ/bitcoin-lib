a2b7941c08e950dc0f0659b827da656d3b4e9070
sstone <fabrice.drouin@acinq.fr>
2021-11-22 16:57:47 +0100

    Use bitcoin-kmp 0.8.2, and remove unused methods

--- src/main/scala/fr/acinq/bitcoinscala/package.scala
+++ src/main/scala/fr/acinq/bitcoinscala/package.scala
@@ -49,43 +49,6 @@ package object bitcoinscala {
   implicit def millibtc2btc(input: MilliBtc): Btc = input.toBtc
   // @formatter:on
 
-  /**
-   * @param input compact size encoded integer as used to encode proof-of-work difficulty target
-   * @return a (result, isNegative, overflow) tuple were result is the decoded integer
-   */
-  def decodeCompact(input: Long): (BigInteger, Boolean, Boolean) = {
-    val nSize = (input >> 24).toInt
-    val (nWord, result) = if (nSize <= 3) {
-      val nWord1 = (input & 0x007fffffL) >> 8 * (3 - nSize)
-      (nWord1, BigInteger.valueOf(nWord1))
-    } else {
-      val nWord1 = input & 0x007fffffL
-      (nWord1, BigInteger.valueOf(nWord1).shiftLeft(8 * (nSize - 3)))
-    }
-    val isNegative = nWord != 0 && (input & 0x00800000) != 0
-    val overflow = nWord != 0 && ((nSize > 34) || (nWord > 0xff && nSize > 33) || (nWord > 0xffff && nSize > 32))
-    (result, isNegative, overflow)
-  }
-
-  /**
-   * @param value input value
-   * @return the compact encoding of the input value. this is used to encode proof-of-work target into the `bits`
-   *         block header field
-   */
-  def encodeCompact(value: BigInteger): Long = {
-    var size = value.toByteArray.length
-    var compact = if (size <= 3) value.longValue << 8 * (3 - size) else value.shiftRight(8 * (size - 3)).longValue
-    // The 0x00800000 bit denotes the sign.
-    // Thus, if it is already set, divide the mantissa by 256 and increase the exponent.
-    if ((compact & 0x00800000L) != 0) {
-      compact >>= 8
-      size += 1
-    }
-    compact |= size << 24
-    compact |= (if (value.signum() == -1) 0x00800000 else 0)
-    compact
-  }
-
   def isAnyoneCanPay(sighashType: Int): Boolean = (sighashType & bitcoin.SigHash.SIGHASH_ANYONECANPAY) != 0
 
   def isHashSingle(sighashType: Int): Boolean = (sighashType & 0x1f) == bitcoin.SigHash.SIGHASH_SINGLE
