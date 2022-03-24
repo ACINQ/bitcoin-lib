55a91b57bd0927c966b6ed59b3550a7c288c4f82
sstone <fabrice.drouin@acinq.fr>
2022-03-24 16:46:24 +0100

    Use bitcoin-kmp 0.8.3

--- src/main/scala/fr/acinq/bitcoin/scalacompat/MerkleTree.scala
+++ src/main/scala/fr/acinq/bitcoin/scalacompat/MerkleTree.scala
@@ -8,5 +8,5 @@ import scala.jdk.CollectionConverters.SeqHasAsJava
   * see https://en.bitcoin.it/wiki/Protocol_specification#Merkle_Trees
   */
 object MerkleTree {
-  def computeRoot(tree: Seq[ByteVector32]): ByteVector32 = fr.acinq.bitcoin.MerkleTree.INSTANCE.computeRoot(tree.map(scala2kmp).asJava)
+  def computeRoot(tree: Seq[ByteVector32]): ByteVector32 = fr.acinq.bitcoin.MerkleTree.computeRoot(tree.map(scala2kmp).asJava)
 }
